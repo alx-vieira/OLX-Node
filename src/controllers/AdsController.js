7b680a14de154026b2baa5bd13080f24b2d2898d
alx-vieira <alxsv@hotmail.com>
2022-09-13 19:49:23 -0300

    Categories List

--- src/controllers/AdsController.js
+++ src/controllers/AdsController.js
@@ -1,7 +1,21 @@
+const Category = require('../models/Category');
+
 module.exports = {
 
     getCategories: async (req, res) => {
+        const cats = await Category.find();
 
+        let categories = [];
+
+        for(let i in cats) {
+            
+            categories.push({
+                ...cats[i]._doc,
+                img: `${process.env.BASE}/assets/images/${cats[i].slug}.png`
+            });
+        }
+
+        res.json({ categories });
     },
 
     addAction: async (req, res) => {
